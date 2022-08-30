96e68a91dac5c2512b07965698ddc2aa166bc235
all-autoo <xiaozhenoh@163.com>
2022-08-30 20:04:32 +0800

    发包

---
+++ assets/common.cb7de00d.js
@@ -0,0 +1,1 @@
+const u=[{label:"\u6B63\u5E38",value:0},{label:"\u51BB\u7ED3",value:1},{label:"\u5220\u9664",value:2}],a=[{label:"\u505C\u7528",value:0},{label:"\u6B63\u5E38",value:1}];export{a as m,u};
--- assets/index.f5781167.js
+++
@@ -1,1 +0,0 @@
-import{L as u,cg as c}from"./index.062059f7.js";var v={beforeMount(n,t){let e=null,a;const l=()=>t.value&&t.value(),o=()=>{Date.now()-a<100&&l(),clearInterval(e),e=null};u(n,"mousedown",r=>{r.button===0&&(a=Date.now(),c(document,"mouseup",o),clearInterval(e),e=setInterval(l,100))})}};export{v};
