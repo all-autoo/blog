c6b441c0c5ab5a41968b2a0f9899e9719501c1d7
all-autoo <xiaozhenoh@163.com>
2022-08-30 17:18:29 +0800

    提交代码

--- assets/dict.8a8b0027.js
+++
@@ -1,1 +0,0 @@
-import{bu as s}from"./index.a7d776d7.js";const i=t=>s.get("/sys/dict/type/"+t),c=t=>t.id?s.put("/sys/dict/type",t):s.post("/sys/dict/type",t),u=t=>s.get("/sys/dict/data/"+t),p=t=>t.id?s.put("/sys/dict/data",t):s.post("/sys/dict/data",t);export{p as a,i as b,c,u};
--- assets/home.98411250.js
+++
@@ -1,1 +0,0 @@
-import{o as a,m as o,p as t,an as _}from"./index.a7d776d7.js";import{E as r}from"./card.4593639a.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const c={},n=_(" 1111 ");function m(d,l){const e=r;return a(),o(e,{class:"layout-home"},{default:t(()=>[n]),_:1})}var f=s(c,[["render",m],["__scopeId","data-v-44211a70"]]);export{f as default};
